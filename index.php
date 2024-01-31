e3ff4bdb68f9c9c2cbeecf37a4d2a5cc45782eaa
Gyasilarbi <gyasilarbi459@gmail.com>
2024-01-31 15:07:33 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>TradeTrove</title>
+    <meta content="Connecting you with skilled professionals for carpentry, electrical work, mechanics, and plumbing services." name="description">
+    <meta content="trades, services, carpenter, electrician, mechanic, plumber" name="keywords">
+
+
+    <!-- Favicons -->
+    <link href="Images/logo.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/styles.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+        <div class="hero-container">
+            <div id="heroCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+
+                <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+                <div class="carousel-inner" role="listbox">
+
+                    <!-- Slide 1 -->
+                    <div class="carousel-item active" style="background-image: url(Images/carpenter.webp);">
+                        <div class="carousel-container">
+                            <div class="carousel-content">
+                                <h2 class="animate__animated animate__fadeInDown">Need a <span>Carpenter?</span></h2>
+                                <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Click Here</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Slide 2 -->
+                    <div class="carousel-item" style="background-image: url(Images/electrician.jpeg);">
+                        <div class="carousel-container">
+                            <div class="carousel-content">
+                                <h2 class="animate__animated animate__fadeInDown">Need an <span>Electrician?</span></h2>
+                                \ <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Click Here</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Slide 3 -->
+                    <div class="carousel-item" style="background-image: url(Images/mechanic.jpeg);">
+                        <div class="carousel-container">
+                            <div class="carousel-content">
+                                <h2 class="animate__animated animate__fadeInDown">Your <span>Mechanic</span> is a click away from you</h2>
+                                <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Click Here</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Slide 4 -->
+                    <div class="carousel-item" style="background-image: url(Images/plumber.jpeg);">
+                        <div class="carousel-container">
+                            <div class="carousel-content">
+                                <h2 class="animate__animated animate__fadeInDown">Do you need a <span>Plumber?</span></h2>
+                                <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Click Here</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon bi bi-chevron-double-left" aria-hidden="true"></span>
+                </a>
+
+                <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon bi bi-chevron-double-right" aria-hidden="true"></span>
+                </a>
+
+            </div>
+        </div>
+    </section><!-- End Hero -->
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="d-flex align-items-center">
+        <div class="container d-flex align-items-center justify-content-between">
+
+            <div class="logo">
+                <a href="index.php"><img src="Images/logo.png" alt="" class="img-fluid"></a>
+            </div>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                    <li class="dropdown"><a class="nav-link scrollto" href="#services"><span>Services</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Electrical Services</a></li>
+                            <li><a href="#">Mechanical Services</a></li>
+                            <li><a href="#">Cleaning Services</a></li>
+                            <li><a href="#">Home Services</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="nav-link scrollto" href="#team">Help and Support</a></li>
+                    <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+
+    <main id="main">
+        <!-- ======= Home======= -->
+        <section id="home" class="home">
+            <div class="container">
+
+                <div class="section-title">
+                    <h1>TradeTrove</h1>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="bucket"></div>
+                        <img src="Images/logo.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-lg-6 pt-4 pt-lg-0 content">
+                        <h4>"TradeTrove is a secure online platform designed to empower tradespeople by offering a dynamic space to showcase their diverse skills and services. <br> Simultaneously, it provides consumers with access to a network of skilled professionals capable of efficiently addressing their fundamental needs. <br> Our platform prioritizes user safety, seamless communication, and problem-solving efficiency, creating a trusted environment where tradespeople and consumers can connect, collaborate, and solve everyday challenges. <br> TradeTrove is not just a platform; it's a solution hub where tradespeople shine, and users discover the expertise they need."</h4>
+                    </div>
+                </div>
+
+            </div>
+        </section><!-- End About Us Section -->
+
+        <!-- ======= Counts Section ======= -->
+        <section class="counts section-bg">
+            <div class="container">
+
+                <div class="row no-gutters">
+
+                    <div class="col-lg-4 col-md-6 d-md-flex align-items-md-stretch">
+                        <div class="count-box">
+                            <i class="bi bi-emoji-smile"></i>
+                            <span data-purecounter-start="0" data-purecounter-end="41572828" data-purecounter-duration="1" class="purecounter"></span>
+                            <p>Skilled <strong>Personel</strong></p>
+                            <p>Discover unparalleled expertise at TradeTrove! Our skilled personnel bring a wealth of experience and proficiency to every task. From seasoned carpenters and plumbers to expert electricians, our professionals are dedicated to solving your challenges with precision and professionalism. Explore a diverse pool of talent on TradeTrove and let our skilled personnel turn your visions into reality. Your projects, our expertise — a perfect match!</p>
+                            <a href="#">Find out more &raquo;</a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 d-md-flex align-items-md-stretch">
+                        <div class="count-box">
+                            <i class="bi bi-globe-europe-africa"></i>
+                            <span data-purecounter-start="0" data-purecounter-end="24" data-purecounter-duration="1" class="purecounter"></span>
+                            <p>"TradeTrove: Unleashing Skills Worldwide! 🌐 Proudly serving across numerous countries, our platform connects skilled tradespeople with consumers globally. Whether you're in bustling cities or serene landscapes, TradeTrove spans borders to bring you a world of expertise. Join us in revolutionizing the way trades are showcased and services are accessed. Wherever you are, TradeTrove is your go-to destination for skilled professionals. Your needs, our global reach — because excellence knows no boundaries!"</p>
+                            <a href="https://www.google.com/search?q=ghana%27s+animal+species&rlz=1C5CHFA_enGH1076GH1077&oq=ghana%27s+animal+species+&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIICAEQABgWGB4yDQgCEAAYhgMYgAQYigUyDQgDEAAYhgMYgAQYigUyDQgEEAAYhgMYgAQYigXSAQkxNTE2MWoxajeoAgCwAgA&sourceid=chrome&ie=UTF-8">Find out more &raquo;</a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 d-md-flex align-items-md-stretch">
+                        <div class="count-box">
+                            <i class="bi bi-people"></i>
+                            <span data-purecounter-start="0" data-purecounter-end="9967" data-purecounter-duration="1" class="purecounter"></span>
+                            <p>A"At TradeTrove, our users are the heartbeat of our community! 🌟 Whether you're a skilled tradesperson showcasing your craft or a savvy consumer seeking solutions, you're part of a dynamic network that values expertise and collaboration. Our platform is fueled by the passion and diversity of users who make every project, every connection, and every solution possible. Join the TradeTrove family — where skills meet needs, and every user contributes to a thriving community of excellence. Together, we build, connect, and elevate!"</p>
+                            <a href="https://www.statista.com/statistics/1230202/number-of-international-tourists-arriving-in-ghana/#:~:text=Number%20of%20tourist%20arrivals%20in%20Ghana%202016%2D2022&text=In%202021%2C%20Ghana%20registered%20around,%2D19)%20in%20the%20country.">Find out more &raquo;</a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Counts Section -->
+
+        <!-- ======= Our Services Section ======= -->
+        <section id="services" class="services">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>Our Services</h2>
+                    <p>
+                        TradeTrove is a comprehensive online platform that bridges the gap between skilled tradespeople and consumers, offering a dynamic space for professionals to showcase their expertise in carpentry, electrical work, plumbing, mechanics, and more. Through secure communication channels and seamless interactions, consumers can discover, connect with, and hire tradespeople for their specific needs. The platform prioritizes user safety, fosters trust through a robust review system, and serves as a solution hub where tradespeople shine and users find the skilled professionals they require. TradeTrove goes beyond being a conventional platform, creating a collaborative environment that facilitates problem-solving, community building, and efficient connections in the trades industry.</p>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                        <div class="icon-box">
+                            <div class="icon"><i class="bi bi-tv-fill"></i></div>
+                            <h4 class="title"><a href="services/tour.php">Electrical Services</a></h4>
+                            <p class="description">Electrical and Mechanical Services cover a broad range of services related to electrical and mechanical systems. Whether for residential, commercial, or industrial settings, these services are crucial for ensuring the functionality, safety, and efficiency of various systems and equipment, etc</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                        <div class="icon-box">
+                            <div class="icon"><i class="bi bi-house"></i></div>
+                            <h4 class="title"><a href="/services/tour.php">Home Services</a></h4>
+                            <p class="description">Routine maintenance tasks to keep the home in good condition.
+                                 Plumbing maintenance, electrical system checks, HVAC system servicing, roof inspections, and gutter cleaning.Professional cleaning services for various areas of the home.
+                                 House cleaning, carpet cleaning, window cleaning, and deep cleaning services, etc</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                        <div class="icon-box">
+                            <div class="icon"><i class="bi bi-car-front-fill"></i></div>
+                            <h4 class="title"><a href="">Mechanical Services</a></h4>
+                            <p class="description">HVAC system installation, maintenance, and repair.
+Boiler installation and servicing.
+Plumbing system installations and repairs.
+Ventilation system design and maintenance.
+Refrigeration system services.
+Mechanical equipment troubleshooting and diagnostics.
+Preventive maintenance for machinery and systems.
+Energy efficiency assessments.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                        <div class="icon-box">
+                            <div class="icon"><i class="bi bi-scissors"></i></div>
+                            <h4 class="title"><a href="">Dining and Culinary Experiences and Events</a></h4>
+                            <p class="description">Recommendations for local restaurants, street food experiences, and cultural dining events. <br> Coordination of special events, celebrations, or group activities.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Our Services Section -->
+
+    </main>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
